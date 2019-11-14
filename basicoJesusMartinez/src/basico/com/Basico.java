dcb9896130eb1bbaa09df625e5077bd90c2bf6d9
Jesus Martinez <jesusmartinezbeltran92@gmail.com>
2019-11-14 11:02:01 +0100

    Añadido metodo cadenas

--- basicoJesusMartinez/src/basico/com/Basico.java
+++ basicoJesusMartinez/src/basico/com/Basico.java
@@ -2,4 +2,13 @@ package basico.com;
 
 public class Basico {
 
-}
+	public static String cadenas() {
+		String cadena="Esto es el metodo cadenas";
+		return cadena;
+	}
+	public static void main (String []Args) {
+		System.out.println(cadenas());
+	}
+	}
+	
+

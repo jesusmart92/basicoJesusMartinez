63f8e95366b0a1644dae23668e4c655a03411a81
Jesus Martinez <jesusmartinezbeltran92@gmail.com>
2019-11-14 11:10:43 +0100

    Metodo logico añadido

--- basicoJesusMartinez/src/basico/com/Basico.java
+++ basicoJesusMartinez/src/basico/com/Basico.java
@@ -2,4 +2,15 @@ package basico.com;
 
 public class Basico {
 
+	public static String logico(int a, int b) {
+		String resultado=null;
+		if (a > b) {
+			resultado= a+" Es mayor que "+b;
+		}
+		else {resultado= b+" Es mayor que "+a;}
+		return resultado;
+}
+	public static void main ( String[] Args) {
+		System.out.println(logico(4,5));
+	}
 }
